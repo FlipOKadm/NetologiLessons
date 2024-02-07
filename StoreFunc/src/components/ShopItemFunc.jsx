fe3e9f93fe29c760ce00650c4f0212503e5a18aa
FlipOK <flipok2@gmail.com>
2024-02-07 20:40:25 +0300

    Destructure props

--- StoreFunc/src/components/ShopItemFunc.jsx
+++ StoreFunc/src/components/ShopItemFunc.jsx
@@ -3,18 +3,18 @@ import CardObject from "./CardObject.jsx";
 
 function ShopItemFunc(props) {
     /*PropTypes.checkPropTypes(ShopItemFunc.props, props,'props','ShopItemFunc')*/
-
+    const {item} = props;
 
     return (
         <div className="main-content">
-            <h2>{props.item.brand}</h2>
-            <h1>{props.item.title}</h1>
-            <h3>{props.item.description}</h3>
-            <div className="description">{props.item.descriptionFull}</div>
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div className="description">{item.descriptionFull}</div>
             <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
             <div className="divider"></div>
             <div className="purchase-info">
-                <div className="price">{props.item.currency}{props.item.price}</div>
+                <div className="price">{item.currency}{item.price}</div>
                 <button>Добавить в корзину</button>
             </div>
         </div>
