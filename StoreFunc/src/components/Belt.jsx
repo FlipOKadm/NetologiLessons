a891ea8998c19218cf9476698ce519fe64d2d638
FlipOK <flipok2@gmail.com>
2024-02-11 16:16:03 +0300

    deleted files

--- StoreFunc/src/components/Belt.jsx
+++
@@ -1,9 +0,0 @@
-export function Belt() {
-    return (
-        <div className="ui-datepicker-header">
-            <div className="ui-datepicker-title">
-                <span className="ui-datepicker-month">Март</span>&nbsp;<span className="ui-datepicker-year">2017</span>
-            </div>
-        </div>
-    )
-}
